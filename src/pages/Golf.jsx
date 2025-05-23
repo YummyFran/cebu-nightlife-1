4c71b88ffd1bb94eec5f9d9fc69d189f6753d8bc
Yummy Fran Palomares <yumpalomares@gmail.com>
2025-05-23 23:03:52 +0800

    change

--- src/pages/Golf.jsx
+++ src/pages/Golf.jsx
@@ -4,7 +4,7 @@ import cardsData from '../data/cards.json'
 const Golf = () => {
   return (
     <main>
-        <title>세부 골프패키지 & 혼체골프패키지 | 세부밤문화No.1</title>  
+        <title>세부 골프패키지 & 황제골프패키지 | 세부밤문화No.1</title>  
         <meta name="description" content="세부 골프패키지 & 혼체골프패키지 완벽 안내! 황제 골프 일정과 포함사항, 인기 골프클럽 소개, 유용한 골프 팁까지 한눈에 확인해보세요." />
         <meta property="og:title" content="세부 골프패키지 & 혼체골프패키지 | 세부밤문화No.1" />
         <meta property="og:description" content="세부 골프패키지 & 혼체골프패키지 완벽 안내! 황제 골프 일정과 포함사항, 인기 골프클럽 소개, 유용한 골프 팁까지 한눈에 확인해보세요." />
