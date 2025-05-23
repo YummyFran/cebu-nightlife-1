992708ac4f453a5a0bc173a3f18437ec9a8250ee
Yummy Fran Palomares <yumpalomares@gmail.com>
2025-05-23 20:27:22 +0800

    change

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -36,7 +36,9 @@ const Homepage = () => {
         </Carousel>
         <Carousel images={carouserImagesMobile} className="mobile">
             <header className="hero">
-                <h1 className='heading'>세부의 밤을 즐기는 가장 <br/>완벽한 방법</h1>
+                <h1 className='heading'>세부밤문화 정보를 찾는 중이신가요?</h1>
+                <h2 className="subheading">지인 혹 친구들과 가는여행 잊지못할 밤문화여행 하셔야 겠지요.
+필리핀 치안,밤문화 불안감 등은 접어두시고 환상의섬 세부에서 힐링여행 어떠신가요?</h2>
             </header>
         </Carousel>
     </main>
