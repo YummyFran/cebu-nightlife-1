210d227530a4fad154bf3e2ec8d81cf327d57525
Yummy Fran Palomares <yumpalomares@gmail.com>
2025-05-23 22:28:02 +0800

    commit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,6 +3,7 @@ import { NavLink, useLocation } from 'react-router-dom'
 import '../styles/navigation.css'
 import { IoMenu } from "react-icons/io5";
 import Logo from "../assets/Logo.svg"
+import Pointer from "../assets/pointer.png"
 
 const Navigation = ({ className }) => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
@@ -32,31 +33,31 @@ const Navigation = ({ className }) => {
                 <div className="close" onClick={() => setIsMobileMenuOpen(false)}>&times;</div>
                 <NavLink to={'/poolvilla'} className={({isActive}) => isActive ? 'active-link' : ''}>
                     <div className="pointer">
-                        <img src="src/assets/pointer.png" alt="" />
+                        <img src={Pointer} alt="" />
                     </div>
                     세부풀빌라
                 </NavLink>
                 <NavLink to={'/escortgirl'} className={({isActive}) => isActive ? 'active-link' : ''}>
                     <div className="pointer">
-                        <img src="src/assets/pointer.png" alt="" />
+                        <img src={Pointer} alt="" />
                     </div>
                     세부에스코트걸
                 </NavLink>
                 <NavLink to={'/golf'} className={({isActive}) => isActive ? 'active-link' : ''}>
                     <div className="pointer">
-                        <img src="src/assets/pointer.png" alt="" />
+                        <img src={Pointer} alt="" />
                     </div>
                     세부골프
                 </NavLink>
                 <NavLink to={'/nightlife'} className={({isActive}) => isActive ? 'active-link' : ''}>
                     <div className="pointer">
-                        <img src="src/assets/pointer.png" alt="" />
+                        <img src={Pointer} alt="" />
                     </div>
                     세부밤문화정보
                 </NavLink>
                 <NavLink to={'/inquiry'} className={({isActive}) => isActive ? 'active-link' : ''}>
                     <div className="pointer">
-                        <img src="src/assets/pointer.png" alt="" />
+                        <img src={Pointer} alt="" />
                     </div>
                     1:1문의
                 </NavLink>
