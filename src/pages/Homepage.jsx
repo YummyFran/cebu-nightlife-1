1a5bdf8b8346966e7e5b2c19ee965b4187236a0a
Yummy Fran Palomares <yumpalomares@gmail.com>
2025-05-25 07:26:04 +0800

    title

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -38,9 +38,9 @@ const Homepage = () => {
   return (
     <main id='homepage'>
             <title>세부밤문화넘버원</title>  
-            <meta name="description" 
+            {/* <meta name="description" 
               content="세부밤문화 풀빌라패키지 , 세부에코걸, 세부에스코트걸, 세부황제골프, 세부 최신 황제투어에 대한 총 정리" 
-            />
+            /> */}
             {/* <meta property="og:title" content="세부에코걸 & 황제투어에 대한 총 정리" />
             <meta property="og:description" content="세부밤문화 풀빌라패키지 , 세부에코걸, 세부에스코트걸, 세부황제골프, 세부 최신 황제투어에 대한 총 정리" />
             <meta property="og:image" content="https://www.cebueco.com/og-image.jpg" /> */}
