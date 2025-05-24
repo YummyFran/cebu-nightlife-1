0dc8202f49b6436d8af27214ce659ac29fc18610
Yummy Fran Palomares <yumpalomares@gmail.com>
2025-05-24 15:25:42 +0800

    seo fix

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,9 +2,9 @@ import Carousel from '../components/Carousel'
 import Banner1 from '../assets/banner1.jpg'
 import Banner2 from '../assets/banner4.jpg'
 import BannerMobile from '../assets/banner-mobile.jpg'
-import TelegramBtn from '../components/Telegrambtn'
 import KakaoBtnWhite from '../components/KakaoBtnWhite'
 import TelegramBtnWhite from '../components/TelegrambtnWhite'
+import { useEffect, useState } from 'react'
 
 const carouselImages = [
   {
@@ -23,32 +23,26 @@ const carouserImagesMobile = [
   }
 ]
 
+const breakpoint = 768
+
 const Homepage = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const checkMobile = () => setIsMobile(window.innerWidth <= breakpoint);
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+    return () => window.removeEventListener('resize', checkMobile);
+  }, [breakpoint])
 
   return (
     <main id='homepage'>
             <title>세부밤문화No.1</title>  
             <meta name="description" content="세부 풀빌라 패키지와 황제 패키지 투어 설명 이미지 – 야간 수영장 사진과 여행 일정표 포함, 카카오톡 및 텔레그램 상담 안내 포함" />
-            <meta property="og:title" content="세부밤문화No.1" />
+            {/* <meta property="og:title" content="세부밤문화No.1" />
             <meta property="og:description" content="세부 풀빌라 패키지와 황제 패키지 투어 설명 이미지 – 야간 수영장 사진과 여행 일정표 포함, 카카오톡 및 텔레그램 상담 안내 포함" />
-            <meta property="og:image" content="https://cebu-nightlife-1.vercel.app/og-image.jpg" />
-        <Carousel images={carouselImages} className="desktop">
-            <header className="hero">
-                <h1 className='heading'><span className='black'>세부밤문화</span> 정보를 찾는 중이신가요?</h1>
-                <h2 className="subheading">
-                  지인 혹 친구들과 가는여행 잊지못할 밤문화여행 하셔야 겠지요.
-                  <br/>
-                  필리핀 치안,밤문화 불안감 등은 접어두시고 
-                  <br />
-                  환상의섬 세부에서 힐링여행 어떠신가요?
-                </h2>
-                <div className="cta">
-                  <KakaoBtnWhite />
-                  <TelegramBtnWhite />
-                </div>
-            </header>
-        </Carousel>
-        <Carousel images={carouserImagesMobile} className="mobile">
+            <meta property="og:image" content="https://cebu-nightlife-1.vercel.app/og-image.jpg" /> */}
+        <Carousel images={isMobile ? carouserImagesMobile : carouselImages}>
             <header className="hero">
                 <h1 className='heading'><span className='black'>세부밤문화</span> 정보를 찾는 중이신가요?</h1>
                 <h2 className="subheading">
