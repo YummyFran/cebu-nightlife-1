4fdc00e12a577b8ca03e753cb19ec3cb40854006
YummyFran <yumpalomares@gmail.com>
2026-03-22 18:32:46 +0800

    headings

--- src/pages/PoolVilla.jsx
+++ src/pages/PoolVilla.jsx
@@ -6,15 +6,15 @@ const PoolVilla = () => {
 
   return (
     <main>
-      <title>세부풀빌라패키지 & 세부황제패키지 | 세부밤문화투어</title>  
+      <title>세부황제패키지 & 세부풀빌라패키지 | 세부밤문화투어</title>  
       <meta name="description" content="세부 풀빌라 패키지와 황제 패키지 투어 설명 이미지 – 야간 수영장 사진과 여행 일정표 포함, 카카오톡 및 텔레그램 상담 안내 포함" />
-      <meta property="og:title" content="세부풀빌라패키지 & 세부황제패키지 | 세부밤문화투어" />
+      <meta property="og:title" content="세부황제패키지 & 세부풀빌라패키지 | 세부밤문화투어" />
       <meta property="og:description" content="세부 풀빌라 패키지와 황제 패키지 투어 설명 이미지 – 야간 수영장 사진과 여행 일정표 포함, 카카오톡 및 텔레그램 상담 안내 포함" />
       <meta property="og:image" content="https://www.cebunightguide.com/og-image.jpg" />
       <link rel="canonical" href="https://www.cebunightguide.com" />
 
       <div className="container">
-        <h1 className='heading'>세부풀빌라패키지 & 세부황제패키지</h1>
+        <h1 className='heading'>세부황제패키지 & 세부풀빌라패키지</h1>
         <TwoCardLayout cards={cardsData.poolvilla}/>  
       </div> 
     </main>
