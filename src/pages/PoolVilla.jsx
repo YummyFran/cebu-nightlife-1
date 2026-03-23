7aac65b212f5a97d00d3136da5c81337c4616f17
YummyFran <yumpalomares@gmail.com>
2026-03-23 09:14:54 +0800

    cannonical poolvilla

--- src/pages/PoolVilla.jsx
+++ src/pages/PoolVilla.jsx
@@ -11,7 +11,7 @@ const PoolVilla = () => {
       <meta property="og:title" content="세부황제패키지 & 세부풀빌라패키지 | 세부밤문화투어" />
       <meta property="og:description" content="세부 풀빌라 패키지와 황제 패키지 투어 설명 이미지 – 야간 수영장 사진과 여행 일정표 포함, 카카오톡 및 텔레그램 상담 안내 포함" />
       <meta property="og:image" content="https://www.cebunightguide.com/og-image.jpg" />
-      <link rel="canonical" href="https://www.cebunightguide.com" />
+      <link rel="canonical" href="https://www.cebunightguide.com/poolvilla" />
 
       <div className="container">
         <h1 className='heading'>세부황제패키지 & 세부풀빌라패키지</h1>
